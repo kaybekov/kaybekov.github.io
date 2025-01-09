6e514990fd056e2e58cb427488d4c38e02a0d5f4
kayumbekov <kayumbekow@gmail.com>
2025-01-09 02:54:59 +0100

    fixed progress bar

--- src/components/home/SkillsBar.jsx
+++ src/components/home/SkillsBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import ProgressBar from "react-bootstrap/ProgressBar";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 function SkillsBar({ skill, value, isScrolled }) {
   return (
     <div style={{ width: "95%" }}>
       <p className="lead mb-1 mt-2">{skill}</p>
-      {/* <ProgressBar
+      <ProgressBar
         className={!isScrolled ? "progress" : "progress-bar-animation"}
         now={value}
-      /> */}
+      />
     </div>
   );
 }
